ea3f48be06a84150c1532547529aa26487f78dc1
Madarier <erkam-babayigit@hotmail.de>
2024-12-17 11:18:48 +0100

    datenbank eingebunden

--- ProNatur-BioMarkt GmbH/ProductScreen.cs
+++ ProNatur-BioMarkt GmbH/ProductScreen.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.Remoting.Contexts;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,13 +14,21 @@ namespace ProNatur_BioMarkt_GmbH
 {
     public partial class ProductScreen : Form
     {
+        private SqlConnection databaseConnection = new SqlConnection(@"Data Source = (LocalDB)\MSSQLLocalDB; AttachDbFilename=C:\Users\Madarier\Documents\ProNatur-Biomarkt GmbH.mdf;Integrated Security = True; Connect Timeout = 30");
+
         public ProductScreen()
         {
             InitializeComponent();
+
+            // Start
+
         }
 
         private void btnProductSave_Click(object sender, EventArgs e)
         {
+            string productName = textBoxProductName.Text;
+
+            // save product name in database
 
         }
 
